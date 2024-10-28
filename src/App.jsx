017e0c12c80ce8b4c444fe6ab1e306fb22383d22
Pamella Lima <pamellafernandes119@outlook.com>
2024-10-28 20:55:23 -0300

    feat: Init frontend-app-nutep

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { createContext } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+
+const MyContext = createContext();
+
+function App() {
+
+  
+  const values={}
+
+  return (
+    <>
+    <BrowserRouter>
+      <MyContext.Provider value={values}>
+      <section className='main flex'>
+       <div className='sidebarWrapper w-[20%]'>
+     
+       </div>
+
+       <div className='content_Right w-[80%]'>
+        <Routes>
+          <Route path="/" exact={true} element={"Hello Nutep"}/>
+        </Routes>
+       </div>
+    </section>
+      </MyContext.Provider>
+    </BrowserRouter>
+  
+    </>
+  )
+}
+
+export default App
