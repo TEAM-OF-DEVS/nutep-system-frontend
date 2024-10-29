fc3433ffb8dbdb9498054abd40c6deb149ff59c2
Pamella Lima <pamellafernandes119@outlook.com>
2024-10-28 21:00:26 -0300

    feat: Added to component folder

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className="sidebar fixed top-0 left-0 z-[100] w-[20%]"></div>
+        </>
+    );
+}
+
+export default Sidebar;
