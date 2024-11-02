742cf5fa4bc112bca3373ed788a84f9a9b1c6291
Pamella Lima <pamellafernandes119@outlook.com>
2024-11-01 22:09:13 -0300

    feat: Added component Sidebar and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createContext } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Sidebar from './components/Sidebar/index.jsx';
+import { Header } from './components/Header/index.jsx';
 
 
 const MyContext = createContext();
@@ -13,14 +14,14 @@ function App() {
     <>
     <BrowserRouter>
       <MyContext.Provider value={values}>
-      <section className='main flex'>
-       <div className='sidebarWrapper w-[20%]'>
-     
+      <section className='main flex '>
+       <div className='sidebarWrapper w-[15%]'>
+        <Sidebar/>
        </div>
 
-       <div className='content_Right w-[80%]'>
+       <div className='content_Right w-[85%]'>
         <Routes>
-          <Route path="/" exact={true} element={"Hello Nutep"}/>
+          <Route path="/" exact={true} element={<Header/>}/>
         </Routes>
        </div>
     </section>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,12 +1,58 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
+import Logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import SearchIcon from '@mui/icons-material/Search';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import TimelineIcon from '@mui/icons-material/Timeline';
 
 const Sidebar = () => {
-    return (
-        <>
-            <div className="sidebar fixed top-0 left-0 z-[100] w-[20%]"></div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="sidebar fixed top-0 left-0 z-[100] w-[15%]">
+        <Link to="/">
+          <div className="logoWrapper py-3 px-3">
+            <img src={Logo} className="w-100" />
+          </div>
+        </Link>
+
+        <div className="sidebarTabs pl-4 mt-4 bg-green-600">
+                <Button  className="text-white w-100 justify-start gap-2"><span className="icon w-[20px] h-[20px] flex items-center justify-center rounded-md"><PeopleAltIcon /></span>
+                   Pacientes
+                </Button>
+        </div>
+
+        <div className="sidebarTabs pl-4 mt-4 bg-green-600">
+                <Button  className=" text-white w-100 justify-start gap-2"><span className="icon w-[20px] h-[20px] flex items-center justify-center rounded-md"><NoteAddIcon /></span>
+                    Cadastros
+                </Button>
+        </div>
+
+        <div className="sidebarTabs pl-4 mt-4 bg-green-600">
+                <Button className=" text-white w-100 justify-start gap-2"><span className="icon w-[20px] h-[20px] flex items-center justify-center rounded-md"><SearchIcon /></span>
+                    Consultas
+                </Button>
+        </div>
+
+        <div className="sidebarTabs pl-4  mt-4 bg-green-600 font-bold">
+                <Button className=" text-white w-100 justify-start gap-2"><span className="icon w-[20px] h-[20px] flex items-center justify-center rounded-md"><ContentPasteIcon /></span>
+                    Agenda
+                </Button>
+        </div>
+
+        <div className="sidebarTabs pl-4  mt-4 bg-green-600">
+                <Button className=" text-white w-100 justify-start gap-2"><span className="icon w-[20px] h-[20px] flex items-center justify-center rounded-md"><TimelineIcon /></span>
+                    Relatórios
+                </Button>
+        </div>
+
+
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
