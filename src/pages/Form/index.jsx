ec4221a6c3af978853330ccde1f9adadfe867575
Pamella Lima <pamellafernandes119@outlook.com>
2024-11-03 19:44:32 -0300

    fix: Adjustment the button

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -505,7 +505,7 @@ export function Form() {
     
       <button
         type="button"
-        className="focus:outline-none text-white  bg-green-700 hover:bg-green-800 focus:ring-4font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+        className="focus:outline-none text-white  bg-green-700 hover:bg-green-800 focus:ring-4 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
       >
         Salvar
       </button>
