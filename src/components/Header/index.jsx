320db9a68860c33c71aabacb813a25629f023513
Pamella Lima <pamellafernandes119@outlook.com>
2024-11-03 18:28:36 -0300

    feat: Added form Nutep

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export const Header = () => {
     return (
-        <div className='bg-green-900 flex font-bold w-100 p-8 items-center justify-between'>
-         
-        </div>
+        <div className='bg-green-900 flex font-bold w-100 p-8 items-center justify-between'></div>
     )
 }
