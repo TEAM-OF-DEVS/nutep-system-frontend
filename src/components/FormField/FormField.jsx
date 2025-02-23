0821dd9b5449a4b6dbc8c79a80b24a29239d2985
Javoso <lucasqueirozqxd@gmail.com>
2025-02-22 23:04:05 -0300

    Exclusao de espaços em branco.

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const FormField = ({ isAPI = false, isDisable = false, isGrid = false, isSelect = false,
   name = "", value, label = "", placeholder = label, type = "text", styleClass = "", styleInput = "",
-  options = [], onChange = () => { }, error, displayAttribute = "label", className = ""
+  options = [], onChange = () => { }, onBlur = () => {}, error, displayAttribute = "label", className = ""
 }) => (
   <div className={`${className} 
   ${isGrid ? "disabled:bg-gray-500 flex items-center space-x-2 font-bold text-sm" :
@@ -10,7 +10,7 @@ export const FormField = ({ isAPI = false, isDisable = false, isGrid = false, is
     <label className={isGrid ? `${styleClass} font-bold text-sm text-gray-800 w-1/2` : `${styleClass} font-bold text-sm text-gray-800`}>{label}</label>
     <div className={isGrid ? "" : "w-full max-w-sm min-w-[100%]"}>
       {isSelect ? (
-        <select id={name} name={name} value={value} onChange={onChange} disabled={isDisable}
+        <select id={name} name={name} value={value} onChange={onChange} onBlur={onBlur} disabled={isDisable}
           className={isGrid ? "w-full bg-transparent placeholder:text-slate-300 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
             : "w-full bg-white text-gray-700 text-sm border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-gray-400 "}
         >
@@ -22,7 +22,7 @@ export const FormField = ({ isAPI = false, isDisable = false, isGrid = false, is
           ))}
         </select>
       ) : (
-        <input id={name} name={name} onChange={onChange} value={value}
+        <input id={name} name={name} onChange={onChange} value={value} onBlur={onBlur}
           type={type} placeholder={placeholder} disabled={isDisable}
           className={ type === "checkbox" ? `${styleInput}`  : (isGrid ? `${styleInput} w-full bg-transparent text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 shadow-sm` : `${styleInput} w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow`)}
         />
