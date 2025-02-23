25678580a7f994e09b28ae9988f211baf31173f7
Javoso <lucasqueirozqxd@gmail.com>
2025-02-23 14:46:10 -0300

    Adição da responsividade nos campos da pagina

--- src/pages/FormCadastroDadosNeonatais/index.jsx
+++ src/pages/FormCadastroDadosNeonatais/index.jsx
@@ -158,8 +158,7 @@ export function FormCadastroDadosNeonatais() {
                 <div className="flex items-end justify-end px-8 pt-4 mb-10">
                     <button
                         type="button"
-                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                    >
+                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
                         Salvar
                     </button>
                 </div>
