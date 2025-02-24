7db1e842bc37902a9ec136e25140dcc1386df044
Javoso <lucasqueirozqxd@gmail.com>
2025-02-24 01:01:55 -0300

    Criação dos enus utilizados como lista de Strings na telas.

---
+++ src/models/enum/EnumDadosNeoNatais.js
@@ -0,0 +1,46 @@
+const ResultadoTeste = {
+    ALTERADO: "Alterado",
+    NORMAL: "Normal",
+    NAO_REALIZADO: "Não realizado",
+    SI: "SI"
+  };
+  
+  const ExamesRealizados = {
+    ECOCARDIOGRAMA: "Ecocardiograma",
+    RNM_CEREBRAL: "RNM cerebral",
+    TC_CRANIO: "TC crânio",
+    US_ABDOMINAL: "US abdominal",
+    USTF: "USTF",
+    OUTROS: "Outros",
+    SI: "SI"
+  };
+  
+  const OpcaoSimOuNaoOuSi = {
+    SIM: "Sim",
+    NAO: "Não",
+    SI: "SI"
+  };
+  
+  const TiposDeParto = {
+    CESARIA: "Cesária",
+    FORCEPS: "Fórceps",
+    NATURAL: "Natural",
+    SI: "SI"
+  };
+  
+  const Apresentacao = {
+    CEFALICA: "Cefálica",
+    CORMICA: "Córmica",
+    PELVICA: "Pélvica",
+    OUTRO: "Outro",
+    SI: "SI"
+  };
+  
+  const TiposDeGestacao = {
+    FETO_UNICO: "Feto único",
+    GEMELARIDADE: "Gemelaridade",
+    OUTRO: "Outro",
+    SI: "SI"
+  };
+  
+  export { ResultadoTeste, ExamesRealizados, OpcaoSimOuNaoOuSi, TiposDeParto, Apresentacao, TiposDeGestacao };
