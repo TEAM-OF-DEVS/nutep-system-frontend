66506be3b67f27e79da5a980921b564004b981cd
DiegoLimaDL <diegolima.dldeveloper@gmail.com>
2025-02-25 20:48:21 -0300

    Criar deploy para o React App

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Servir os arquivos da build do React
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Porta dinâmica para Heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
