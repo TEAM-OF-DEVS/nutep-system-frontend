efd095542e2a12874a23ff61a95c3e6f4431789a
DiegoLimaDL <diegolima.dldeveloper@gmail.com>
2025-02-26 00:08:12 -0300

    Criar dirName manualmente

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-const express = require('express')
-const {resolve} = require('path')
-const app = express()
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
+
+const express = require('express');
+const {resolve} = require('path');
+const app = express();
 
 
 app.use('/',
