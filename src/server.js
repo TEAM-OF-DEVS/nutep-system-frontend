9cb828a68840c11889c0886dbfe82bc9d5c20d5a
DiegoLimaDL <diegolima.dldeveloper@gmail.com>
2025-02-25 23:04:57 -0300

    Config express

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const path = require("path");
+const {resolve} = require("path");
 require('dotenv').config();
 
 const app = express();
 
 // Servir os arquivos da build do React
-app.use(express.static(path.join(__dirname, "build")));
+app.use('/',
+express.static(
+    resolve(
+        __dirname,
+        './build'
+        )
+    )
+)
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
