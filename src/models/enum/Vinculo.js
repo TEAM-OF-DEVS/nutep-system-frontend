4edde78cf93d5af50a77370ce64f61b9d3d71f68
Lucas do Nascimento Queiroz <lucasqueirozqxd@gmail.com>
2025-08-08 01:05:10 -0300

    Update Abrigo
    Update Abrigo

--- src/models/enum/Vinculo.js
+++ src/models/enum/Vinculo.js
@@ -1,29 +1,16 @@
 const Vinculo = {
-    ABRIGO: "Abrigo",
-    AVO_MATERNO: "Avô materno",
-    AVO_PATERNO: "Avô paterno",
-    AVO_MATERNA: "Avó materna",
-    AVO_PATERNA: "Avó paterna",
-    IRMA: "Irmã",
-    IRMAO: "Irmão",
-    PRIMA: "Prima",
-    PRIMO: "Primo",
-    TIA: "Tia",
-    TIO: "Tio",
-    OUTROS: "Outros"
-  };
+  ABRIGO: "Abrigo",
+  AVO_MATERNO: "Avô materno",
+  AVO_PATERNO: "Avô paterno",
+  AVO_MATERNA: "Avó materna",
+  AVO_PATERNA: "Avó paterna",
+  IRMA: "Irmã",
+  IRMAO: "Irmão",
+  PRIMA: "Prima",
+  PRIMO: "Primo",
+  TIA: "Tia",
+  TIO: "Tio",
+  OUTROS: "Outros",
+};
 
-  ABRIGO("Abrigo"),
-  AVO_MATERNA("Avó - materna"),
-  AVO_MATERNO("Avô materno"),
-  AVO_PATERNA("Avó - paterna"),
-  AVO_PATERNO("Avô paterno"),
-  IRMA("Irmã"),
-  IRMAO("Irmão"),
-  PRIMA("Prima"),
-  PRIMO("Primo"),
-  TIA("Tia"),
-  TIO("Tio"),
-  OUTROS("Outros");
-
-  export default Vinculo;
+export default Vinculo;
